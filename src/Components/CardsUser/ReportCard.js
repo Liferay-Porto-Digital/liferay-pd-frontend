fa67abd8a0ea657f48a61ed1b513626b0c36b571
icgsbr <igorcostagomessouza@gmail.com>
2022-12-02 14:18:07 -0300

    STYLE [Changed Title Text Cases]

--- src/Components/CardsUser/ReportCard.js
+++ src/Components/CardsUser/ReportCard.js
@@ -17,21 +17,21 @@ function ReportCard() {
                 <div className="col-md-6 container-report">
                     <div className="row report-row">
                         <div className="col-md data">
-                            <p>Total de doações</p>
+                            <p>Total de Doações</p>
                             <h5 classeName="data">{"R$ "+ summary?.amountDonated}</h5>
                         </div>
                         <div className="col-md data">
-                            <p>Total de atividades</p>
+                            <p>Total de Atividades</p>
                             <h5 classeName="data">{summary?.amountDoneActivity + " horas"}</h5>
                         </div>
                     </div>
                     <div className="row report-row">
                         <div id="table-donate" className="col-md data">
-                                <p>Doações por instituição</p>
+                                <p>Doações por Instituição</p>
                                 <h5 classeName="data">{"R$ "+ summary?.donationsPerInstitution}</h5>
                         </div>
                         <div className="col-md data">
-                            <p>Atividade por instituição</p>
+                            <p>Atividade por Instituição</p>
                             <h5 classeName="data">{summary?.activityPerInstitution + " horas"}</h5>
                         </div>
                     </div>
